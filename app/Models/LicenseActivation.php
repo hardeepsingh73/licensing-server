8138bb4f5a0bb5a22bb42e2f58909e31d5777bec
Hardeep Singh <hardeepsc9900@gmail.com>
2025-08-20 11:25:15 +0530

    add logs

--- app/Models/LicenseActivation.php
+++ app/Models/LicenseActivation.php
@@ -2,10 +2,13 @@
 
 namespace App\Models;
 
+use App\Traits\LogsActivity;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\SoftDeletes;
 
 class LicenseActivation extends Model
 {
+    use SoftDeletes, LogsActivity;
 
     protected $fillable = ['license_key_id', 'device_id', 'ip_address', 'user_agent'];
 
